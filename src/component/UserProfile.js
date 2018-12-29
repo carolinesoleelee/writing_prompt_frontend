030786b74d2cd3af199c8a8e679715f182878256
Solee Lee <carolinesoleelee@gmail.com>
2018-12-29 00:26:24 -0500

    added cute weather

--- src/component/UserProfile.js
+++ src/component/UserProfile.js
@@ -3,17 +3,29 @@ import { Card, Image } from "semantic-ui-react";
 import {Redirect} from 'react-router-dom'
 import IndividualPost from './IndividualPost'
 import {Link} from 'react-router-dom'
+import Clock from 'react-live-clock';
+import { OpenWeatherMap } from 'react-weather';
+import ReactAnimatedWeather from 'react-animated-weather';
 
 export default class UserProfile extends Component{
 
 
   render(){
     let currentUser = this.props.currentUser
+    const defaults = {icon: 'RAIN', color: 'black', size: 50,animate: true
+};
     return(
       <div>
       {currentUser ? (
 
         <div>
+        <p>Weather & Time</p><ReactAnimatedWeather
+            icon={defaults.icon}
+            color={defaults.color}
+            size={defaults.size}
+            animate={defaults.animate}
+          />
+       <Clock format={'HH:mm'} ticking={true} timezone={'US/Pacific'} />
             <h1>{currentUser.username}{currentUser.user_type}</h1>
             <Link className='item' to='/'>Time to write</Link>
             <h1>Welcome Back {currentUser.name}!</h1>
@@ -27,3 +39,13 @@ export default class UserProfile extends Component{
 }
 
 //add an avatar
+// CLEAR_DAY
+// CLEAR_NIGHT
+// PARTLY_CLOUDY_DAY
+// PARTLY_CLOUDY_NIGHT
+// CLOUDY
+// RAIN
+// SLEET
+// SNOW
+// WIND
+// FOG
