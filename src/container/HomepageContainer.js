a213457357074167f89f30c7e4a20889ba94a994
Solee Lee <carolinesoleelee@gmail.com>
2018-12-31 17:43:48 -0500

    added css

--- src/container/HomepageContainer.js
+++ src/container/HomepageContainer.js
@@ -6,14 +6,26 @@ export default class HomePageContainer extends Component{
   render(){
     let number = Math.floor(Math.random() * 313)
     return(
+      <div className='Homepage'>
+        <div>
+        <img src='https://i.imgur.com/oRNCJx2.png' width='100%'></img>
+
+        </div>
+
       <div>
-      <h1>Would you like to...</h1>
+      <br /><br />
+      <h1>Writing Edifies <br />The Mind</h1>
+      <p>If you want to become a better writer, the best thing you can do is practice writing every single day. Writing prompts are useful because we know sometimes it can be hard to think of what to write about! To help you brainstorm, we put together this list of 300+ writing prompts to give you something to write about daily and increase your writing potential.</p>
+<div className='buts'>
       <Link className='item' to='/categories'>
-      <button>Choose Your Own Question</button>
-      </Link><br /><br />
+      <button>Choose a Question</button>
+      </Link>
       <Link className='item' to={`/questions/${number}`}>
       <button>Allow Randomize</button>
       </Link>
+</div>
+      </div>
+
       </div>
     )
   }
